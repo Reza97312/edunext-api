9e4ef024b8ccf444d4f157ce09d571c2d36847ee
reza <rezakazemi1381@yahoo.com>
2026-02-04 20:52:37 +0330

    d

--- src/config/uploadConfig.js
+++ src/config/uploadConfig.js
@@ -1,6 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key exist?:", !!process.env.CLOUDINARY_API_KEY);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
