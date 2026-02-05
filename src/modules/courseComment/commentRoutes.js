61ea1846288776b705664d527af43097f37b5039
reza <rezakazemi1381@yahoo.com>
2026-02-05 11:54:44 +0330

    d

--- src/modules/courseComment/commentRoutes.js
+++ src/modules/courseComment/commentRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("./commentController");
-const protect = require("../../middlewares/authMiddleware");
+const { protect } = require("../../middlewares/authMiddleware");
 const authorize = require("../../middlewares/roleMiddleware");
 
 router.post("/:courseId", protect, commentController.createComment);
