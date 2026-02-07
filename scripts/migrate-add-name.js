0e34f194a39edbe937adab6a2feb3cc57dadb081
reza <rezakazemi1381@yahoo.com>
2026-02-07 14:38:14 +0330

    d

---
+++ scripts/migrate-add-name.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const User = require("../src/modules/user/userModel");
+
+const migrate = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    const result = await User.updateMany(
+      { name: { $exists: false } },
+      { $set: { name: "Unknown" } },
+    );
+
+    console.log("Migration done:", result.modifiedCount, "users updated");
+
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+migrate();
