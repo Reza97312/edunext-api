fc5ee75ef82f42ee769f390f81e0a90f4f9fad66
reza <rezakazemi1381@yahoo.com>
2026-02-16 21:16:46 +0330

    d

--- src/app.js
+++ src/app.js
@@ -15,7 +15,10 @@ const courseLevelRoutes = require("./modules/courseLevel/courseLevelRoutes");
 
 const app = express();
 
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
+
+app.use(require("cors")({ origin: true, credentials: true }));
+app.options("*", require("cors")({ origin: true, credentials: true }));
 
 // app.use(
 //   helmet({
@@ -26,51 +29,6 @@ const app = express();
 
 app.use(globalLimiter);
 app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://your-frontend-domain.com"],
-//     credentials: true,
-//   }),
-// );
-
-// const allowedOrigins = [
-//   process.env.CLIENT_URL,
-//   "http://localhost:3000",
-//   "http://127.0.0.1:3000",
-//   "http://192.168.1.105:3000",
-// ].filter(Boolean);
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) !== -1) return callback(null, true);
-//     return callback(new Error("Not allowed by CORS"));
-//   },
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//   allowedHeaders: [
-//     "Content-Type",
-//     "Authorization",
-//     "X-Requested-With",
-//     "Accept",
-//     "Origin",
-//   ],
-//   optionsSuccessStatus: 204,
-// };
-
-// app.use((req, res, next) => {
-//   console.log(
-//     "[REQ]",
-//     req.method,
-//     req.originalUrl,
-//     "Origin:",
-//     req.headers.origin || "<no-origin>",
-//   );
-//   next();
-// });
-
-// app.use(cors(corsOptions));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api/courses", courseRoutes);
