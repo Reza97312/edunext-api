d374a4f544d899082267364b928134d6f2387af8
reza <rezakazemi1381@yahoo.com>
2026-05-03 22:48:23 +0430

    d

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ app.use(
 
 app.use(cookieParser());
 
-app.use(globalLimiter);
+// app.use(globalLimiter);
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
