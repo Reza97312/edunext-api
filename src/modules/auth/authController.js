c280850aceb2e3c6bf71ce84c272c85efc87ac2e
Reza97312 <rezakazemi1381@yahoo.com>
2026-06-07 23:45:54 +0430

    Bug Fixed

--- src/modules/auth/authController.js
+++ src/modules/auth/authController.js
@@ -21,6 +21,7 @@ const login = async (req, res, next) => {
     const cookieOptions = {
       httpOnly: true,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
