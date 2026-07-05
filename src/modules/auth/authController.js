919b232e387b0742dc7f44b077420742fa30a6d1
Reza97312 <rezakazemi1381@yahoo.com>
2026-07-05 09:23:51 +0430

    Update error message

--- src/modules/auth/authController.js
+++ src/modules/auth/authController.js
@@ -80,7 +80,7 @@ const refreshToken = async (req, res, next) => {
     if (!token) {
       return res
         .status(401)
-        .json({ success: false, message: "Please login firstttttttttt" });
+        .json({ success: false, message: "Please log in to continue" });
     }
 
     const result = await authService.refreshAccessToken(token);
